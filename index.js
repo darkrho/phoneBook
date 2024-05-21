8b16d4b130ba65ff01a291dec72608b997eccba0
darkrho <darkrho@outlook.es>
2024-05-21 17:13:47 +0200

    ten build

--- index.js
+++ index.js
@@ -105,4 +105,6 @@ app.post("api/persons", (request, response) => {
 /* connection part */
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
